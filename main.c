fbf58db7f51b631416b7b14045dccfcbf39b3b6d
Rúben Gomes <ruben.gomes16@hotmail.com>
2022-12-17 20:14:18 +0000

    First Commit

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+// ############################ Constantes ############################
+
+
+// ############################ Estrutura ############################
+
+typedef struct {
+
+    char nome[81];
+
+}t_estudante;
+
+
+int main(){
+
+    return 0;
+}
